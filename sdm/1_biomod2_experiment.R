561daa17cfec6822be0e4ed23b06aed595b477e8
Samuel Bosch <samuel.bosch@gmail.com>
2013-06-04 14:54:51 +0200

    first model execution with limited number of environmental variables

--- sdm/1_biomod2_experiment.R
+++ sdm/1_biomod2_experiment.R
@@ -4,8 +4,8 @@
 # Author: Samuel Bosch
 ###############################################################################
 
-library(biomod2)
-require('raster')
+require("biomod2")
+require("raster")
 
 root = "Google Drive\\predictors\\1.tryout"
 
@@ -13,17 +13,38 @@ fucus = read.csv(paste(root, "2.processed\\fucus_serratus_obis_20130530_142854.c
 
 coord = fucus[c('longitude', 'latitude')] ## coordinates for fucus serratus occurrence records
 
+points = SpatialPoints(coord)
+
 marspec.dir = "data\\marspec\\MARSPEC_10m\\ascii"
 
 marspec.vars = list.files(path=marspec.dir, pattern="\\.asc$", full.names=TRUE)
 
-env = stack(marspec.vars)
+#env = stack(marspec.vars)
 
-biomod.data = BIOMOD_FormatingData(resp.var = rep(1, nrow(coord)), ## not entirely sure if this is needed
+env = stack (paste(marspec.dir, "bathy_10m.asc", sep="/"), paste(marspec.dir, "biogeo13_sst_mean_10m.asc", sep="/"))
+
+biomod.data = BIOMOD_FormatingData(resp.var = points,
                                    expl.var = env, # explanatory variable(s)
-                                   resp.xy = coord,
                                    resp.name ="fucus_serratus",
-                                   PA.nb.rep = 10, # select pseudo absence locations 10 times
+                                   resp.xy=coord, # locations
+                                   PA.nb.rep = 1, # number of pseudo absence samplings
                                    PA.nb.absences = 10000, # 10000 pseudo absence locations
                                    PA.strategy = "random", # must be  ‘random’, ‘sre’, ‘disk’ or ‘user.defined’
                                    na.rm = TRUE) # remove points with missing environmental data 
+
+biomod.options = BIOMOD_ModelingOptions()
+
+biomod.model = BIOMOD_Modeling(
+            data=biomod.data,
+            models=c('RF', 'MAXENT'),
+            models.options = biomod.options,
+            NbRunEval=3, 
+            DataSplit=50, 
+            Prevalence=0.5, 
+            VarImport=3, 
+            models.eval.meth = c('TSS','ROC'), 
+            SaveObj = TRUE, 
+            rescal.all.models = TRUE, 
+            do.full.models = FALSE, 
+            modeling.id = paste("fucus_Serratus","20130604",sep="_")
+        )
