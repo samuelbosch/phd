df90a57d59b111002866da96ca39adb49a84838b
Samuel Bosch <samuel.bosch@gmail.com>
2013-09-27 17:32:26 +0200

    Get Paper: Found issue, updated TODO

--- getpaper/scripts/popup.js
+++ getpaper/scripts/popup.js
@@ -2,10 +2,10 @@ function load(){
 	var link = chrome.extension.getBackgroundPage().paperLink;
 	var name = chrome.extension.getBackgroundPage().paperName;
 	if(link) {
-		$("<a/>", { html:"View",href:link, target:"_blank"}).appendTo("#download");
+		$("<a/>", { html:"View",href:popuplink, target:"_blank"}).appendTo("#download");
 		$("<br />").appendTo("#download");
 		// create download link, file extension is .paper instead of .pdf to prevent chrome warnings
-		$("<a/>", { html:"Download", href:link, download: name + ".paper", target:"_blank"}).appendTo("#download");
+		$("<a/>", { html:"Download", href:popuplink, download: name + ".paper", target:"_blank"}).appendTo("#download");
     }
 }
 
