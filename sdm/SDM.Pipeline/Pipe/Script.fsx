db6f8b63ff836debe916718c86e01a38532ffca1
Samuel <samuel.bosch@gmail.com>
2013-10-31 09:52:30 +0100

    interactive script for debugging issues

--- sdm/SDM.Pipeline/Pipe/Script.fsx
+++ sdm/SDM.Pipeline/Pipe/Script.fsx
@@ -1,8 +1,20 @@
 ﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
 // for more guidance on F# programming.
-
-#load "Library1.fs"
-open Pipe
-
 // Define your library scripting code here
 
+#I @"D:\a\Google Drive\code\sdm\SDM.Pipeline\Pipe\bin\Release"
+#r "Pipe"
+open SB.SDM.Pipeline.Pipe
+open PositionsDepth
+#time "on"
+
+
+let (info, data) = List.head rasters;;
+val p : Position list = [{Id = 13369121;
+                          Lon = -122.5216904;
+                          Lat = 36.58480835;}]
+
+RasterCompressed.getValue info data (3449*1<px>) (3206*1<px>);;
+
+let r = Raster.loadAscii """D:\a\data\gebco\gebco_gridone.asc""";;
+r.Rows |> Seq.skip 3205*21601 |> Seq.skip 3448 |> Seq.head;;
