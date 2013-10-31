a8d7f32c581d56a5a85f4a3790edfa97851b455e
Samuel <samuel.bosch@gmail.com>
2013-10-31 09:51:37 +0100

    bat file for converting etopo to ascii

--- rasters/convert_etopo_to_ascii.bat
+++ rasters/convert_etopo_to_ascii.bat
@@ -1,1 +1,1 @@
-gdal_translate -of AAIGrid D:\a\data\Etopo\etopo1_bed_c_f4.flt D:\a\data\Etopo\etopo1_bed_c.asc
+gdal_translate -of AAIGrid D:\a\data\Etopo\etopo1_ice_c_f4.flt D:\a\data\Etopo\etopo1_ice_c.asc
