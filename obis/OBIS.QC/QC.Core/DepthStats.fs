59f3c3747d536ab48a4e8b8121263c1b0a6fb043
Samuel <samuel.bosch@gmail.com>
2013-11-22 10:28:00 +0100

    DepthStats: lower memory usage by splitting functions

--- obis/OBIS.QC/QC.Core/DepthStats.fs
+++ obis/OBIS.QC/QC.Core/DepthStats.fs
@@ -8,6 +8,10 @@ open System.IO
 module Tuple =
     let toArray = Microsoft.FSharp.Reflection.FSharpValue.GetTupleFields
 
+module File = 
+    let writeAllLines path (contents:seq<string>) = 
+        File.WriteAllLines(path, contents)
+
 module DepthStats =
     let applystatfuncs (data:float []) = 
         let inv d f = f d
@@ -25,14 +29,17 @@ module DepthStats =
                                | Some(s) -> string s 
                                | None -> """\N""") // NULL
 
-    let run input output = 
+    let load input = // making more ram friendly by making load a separate function
         File.ReadAllLines(input)
         |> Seq.skip 1 // header
         |> Seq.map ((fun l -> l.Split([|','|])) >> (fun x -> (int x.[0]) , (float x.[1])))
+        |> Array.ofSeq
+
+    let run input output = 
+        load input
         |> Seq.groupBy fst
         |> Seq.map (fun (tnameid, data) -> 
                         let stats = statistics data
                         sprintf "%i,%i,%s" tnameid (Seq.length data) (String.Join(",", stats)))
-        |> fun lines -> (File.WriteAllLines(output, lines))
+        |> File.writeAllLines output
         |> ignore
-
